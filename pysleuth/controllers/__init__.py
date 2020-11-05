b7f706cd4f617d83b939debb68a1783b383aa7ad
Ashwin A Nayar <ashwin5059198@gmail.com>
2020-11-06 00:44:38 +0530

    maintain naming consistency

--- pysleuth/controllers/__init__.py
+++ pysleuth/controllers/__init__.py
@@ -2,4 +2,4 @@ from ._keylogger_ctrl import KeyLoggerController
 from ._process_ctrl import ProcessMntrController
 from ._mouse_ctrl import MouseMntrController
 from ._screen_ctrl import ScreenMntrController
-from ._emailController import EmailController
+from ._email_ctrl import EmailController
