14e58b9415442fca73d70e4503a4047cae3b448f
Ashwin A Nayar <ashwin5059198@gmail.com>
2020-11-05 15:15:13 +0530

    add core mouse monitor

--- pysleuth/core/components/__init__.py
+++ pysleuth/core/components/__init__.py
@@ -1,2 +1,3 @@
 from ._keylogger import KeyLogger
 from ._process_monitor import ProcessMonitor
+from ._mouse_monitor import MouseMonitor
---
+++ pysleuth/core/components/_mouse_monitor.py
@@ -0,0 +1,18 @@
+from pynput import mouse
+
+from ..base import BaseComponent
+from ..signal import Signal
+
+
+class MouseMonitor(BaseComponent):
+    SIG_clicked = Signal(int, int, str, str)
+
+    def __init__(self):
+        super(MouseMonitor, self).__init__()
+
+    def run(self):
+        listener = mouse.Listener(on_click=self._emitSignal)
+        listener.start()
+
+    def _emitSignal(self, x, y, button, pressed):
+        self.SIG_clicked.emit(x, y, str(button), str(pressed))
